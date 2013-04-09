01946e5397139f5a902a501f6cb7f878e121f526
jaejinyun <skplanet@jaejinmac.local>
2013-04-09 16:01:54 +0900

    26번 해결

--- euler26.erl
+++ euler26.erl
@@ -6,11 +6,34 @@
 main()->
     divide(2).
 
-divide(1000)->
-    io:format("~p~n",[1/1000]);
-divide(M) ->
-    io:format("~p~n",[1/M]),
-    divide(M+1).
+divide(D)->
+    divide(1,D,dict:new(),0,1,D,D).
+
+divide(_P,_Q,_Dict,_Max,_Pos,1000,MaxD)->
+    MaxD;
+divide(0,_Q,_Dict,Max,_Pos,D,MaxD)->
+    divide(1,D+1,dict:new(),Max,1,D+1,MaxD);
+divide(P,Q,Dict,Max,Pos,D,MaxD) when P < Q  ->
+    divide(P*10,Q,Dict,Max,Pos,D,MaxD);
+divide(P,Q,Dict,Max,Pos,D,MaxD) ->
+    M = P div Q,
+    Rem = P rem Q,
+    Key = integer_to_list(M) ++ " " ++ integer_to_list(Rem),
+    case dict:find(Key, Dict) of
+        {ok, [Value]} ->
+            Current = Pos - Value,
+            if Max < Current ->
+                    divide(1,D+1,dict:new(),Current,1,D+1,Q);
+               true ->
+                    divide(1,D+1,dict:new(),Max,1,D+1,MaxD)
+            end;
+        error ->
+            NewDict = dict:append(Key,Pos,Dict),
+            divide(Rem,Q,NewDict,Max,Pos+1,D,MaxD)
+    end.
+
+    
+
 
 
 
